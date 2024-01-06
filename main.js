25c448fa82b37a1517590eb5a863179eb5da38df
Gingene <ginx2618@gmail.com>
2024-01-06 15:55:05 +0800

    test path

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+console.log(location.pathname);
 if (location.pathname === "/" || location.pathname === "/index.html") {
+  console.log("test");
   import("./src/assets/components/login.js");
 } else if (location.pathname === "/product.html") {
-  import("./src/assets/components/products.js");
+  import("./src/assets/components/aa/products.js");
 }
