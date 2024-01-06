646408ac7cd87e7d4659390cbab81c6d72dda33b
Gingene <ginx2618@gmail.com>
2024-01-06 15:14:05 +0800

    feat(components): convert data and methods to composition API
    - Converted the data and methods in login.js and products.js components to Vue 3's Composition API for better code organization and reusability. This change also includes using ref, reactive, and onMounted from vue.esm-browser.min.js rather than traditional options and methods.

--- src/assets/components/products.js
+++ src/assets/components/products.js
@@ -1,51 +1,70 @@
-import { createApp } from "../../vue/vue.esm-browser.min.js";
+import { createApp, onMounted, ref } from "../../vue/vue.esm-browser.min.js";
 import { $http, path } from "../api/config.js";
 
 const app = createApp({
-  data() {
-    return {
-      products: [],
-      tempProduct: {},
-    };
-  },
-  methods: {
-    async checkAdmin() {
-      this.loading("讀取資料中，請稍後");
+  setup() {
+    const products = ref([]);
+    const tempProduct = ref({});
+    const load = ref(null);
+
+    // loading modal
+    function loading(msg) {
+      load.value.classList.remove("d-none");
+      load.value.childNodes[0].childNodes[0].textContent = msg;
+    }
+
+    // 移除loading modal
+    function removeloading() {
+      load.value.classList.add("d-none");
+    }
+
+    // 選擇product時，將product資料存入tempProduct中，並將tempProduct傳入product.html中
+    function findProduct(item) {
+      tempProduct.value = item;
+    }
+
+    // 取得product資料，並將products傳入product.html中
+    async function getProducts() {
+      try {
+        const res = await $http.get(`${path.admin}/products`);
+        products.value = res.data.products;
+        removeloading();
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    // 檢查是否為管理者，並取得product資料，並將products傳入product.html中
+    async function checkAdmin() {
+      loading("讀取資料中，請稍後");
       try {
         await $http.post(path.check);
-        this.getProducts();
+        getProducts();
       } catch (err) {
         alert(err.response.data.message);
         location.href = "index.html";
       }
-    },
-    findProduct(item) {
-      this.tempProduct = item;
-    },
-    async getProducts() {
-      try {
-        const res = await $http.get(`${path.admin}/products`);
-        this.products = res.data.products;
-        this.removeloading();
-      } catch (err) {
-        console.error(err);
-      }
-    },
-    loading(msg) {
-      this.$refs.load.classList.remove("d-none");
-      this.$refs.load.childNodes[0].childNodes[0].textContent = msg;
-    },
-    removeloading() {
-      this.$refs.load.classList.add("d-none");
-    },
-  },
-  mounted() {
-    const token = document.cookie.replace(
-      /(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/,
-      "$1"
-    );
-    $http.defaults.headers.common.Authorization = token;
-    this.checkAdmin();
+    }
+
+    // 初始化頁面時，取得cookie中的token，並將token傳入$http.defaults.headers.common.Authorization中
+    onMounted(() => {
+      const token = document.cookie.replace(
+        /(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/,
+        "$1"
+      );
+      $http.defaults.headers.common.Authorization = token;
+      checkAdmin();
+    });
+
+    return {
+      load,
+      products,
+      tempProduct,
+      findProduct,
+      getProducts,
+      loading,
+      removeloading,
+    };
   },
 });
 
